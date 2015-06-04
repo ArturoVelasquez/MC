31765a24b395c8a24292c3c15fccda1e77b5fab0
ArturoVelasquez <arturovgr@gmail.com>
2015-06-03 23:42:29 -0500

    Update bruno.sh

--- Tarea1/bruno.sh
+++ Tarea1/bruno.sh
@@ -13,21 +13,25 @@ awk -F "," '{if ($2 <0.01 && $2 >=0){print  $1}}' kepler.csv >  planetas.csv
 # Cuenta la cantidad de elementos que cumplen con la condicion anterior y la asigana a una variable
 cantidad2=$(wc -l planetas.csv | sed 's/planetas.csv//g' )
 
-#imprime los valores requeridos
+#imprime los elementos con los valores requeridospor masa
 echo 'La cantidad de planetas con masa menor que una centesima de la masa de Jupiter es:' $cantidad2 
 
 echo 'Los planetas son:' 
 
 cat planetas.csv
 
+#Saca los datos para las orbitas por planeta 
 awk -F "," '{if ($6 >=0){print  $6,$1}}' kepler.csv > orbitas.csv
 
+#ordena los planetas por tamaño de orbita
 sort -i orbitas.csv > orbitas1.csv
 
+#Imprime el menor vaor para las orbitas
 echo 'el planeta con menor radio es: '
 
 awk '{print $2,$3}' orbitas1.csv | head -1
 
+# Elimina los archivos generados
 rm orbitas.csv
 rm orbitas1.csv
 rm planetas.csv
