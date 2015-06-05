5f285a13ac35dbdc7421551ebc5b3dd1964a5396
users <a.velasquez11@compufi11.uniandes.edu.co>
2015-06-05 11:55:46 -0500

    primer ejercisio de python terminado

---
+++ python/excercises/pywork-10.py
@@ -0,0 +1,16 @@
+import numpy
+
+a = int(raw_input("please enter integer x:"))
+b = int(raw_input("please enter integer y:"))
+
+suma = a+b
+resta = a-b
+producto = a*b
+cociente = a/b
+modulo = a%b
+logaritmo = numpy.log10(a)
+exponencial = a**b
+
+print suma, resta, producto, cociente, modulo, logaritmo, exponencial
+
+
