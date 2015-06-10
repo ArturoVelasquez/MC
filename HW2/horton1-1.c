d860df651acc3c06c2384420ab1ef3d3b468b597
ArturoVelasquez <a.velasquez11@uniandes.edu.co>
2015-06-09 22:16:13 -0500

    una ligera correccion

--- HW2/horton1-1.c
+++ HW2/horton1-1.c
@@ -3,7 +3,7 @@
 int main(void)
 {
  printf("Mi nombre es Arturo Vel%csquez.",160);
- printf("\n Mi direccion de correo es\: a.velasquez11");
+ printf("\n Mi direccion de correo es: a.velasquez11");
  return 0;
 }
 
