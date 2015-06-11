ea3fc997d74ebe777b4d7a2c2ea519dc1811f5a6
ArturoVelasquez <arturovgr@gmail.com>
2015-06-10 23:52:12 -0500

    Update horton2-3.c

--- HW2/horton2-3.c
+++ HW2/horton2-3.c
@@ -2,26 +2,26 @@
 
 int main(void)
 {
-/*se declaran las variables para la cantidad, el tipo de producto y el total a pagar*/
+/*Se declaran las variables para la cantidad, el tipo de producto, y el total a pagar*/
 	int type=0, quantity=0;
 	float total=0.00; 
 	
-/*Se declaran las consatantes para el valor segun el articulo*/
+/*Se declaran las consatantes para el valor segun el artículo*/
 	const float standar=3.50, deluxe=5.50
 
-/*se pide el tipo de articulo requerido*/
+/*Se pide el tipo de artículo requerido*/
 	printf("\nIntrodusca 1 para el articulo estandar, 2 para el articulo de lujo: ");
 
-/*se asigna el tipo de articulo*/
+/*Se asigna el tipo de artículo*/
 	scanf("%d",&type);
 
-/*se pide la cantidad de articulos requeridos*/
+/*Se pide la cantidad de artículos requeridos*/
 	printf("\nIntrodusca la cantidad de articulos: ");
 
-/*se asigna la cantida de articulos*/
+/*Se asigna la cantida de artículos*/
 	scanf("%d",&quantity);
 
-/* este condicional devuelve el total segun los el tipo de articulo y cantidad requerida*/	
+/*Este condicional devuelve el total según los el tipo de artículo y cantidad requerida*/	
 	if(type==1)
 		{
 		total = quantity*standar;
