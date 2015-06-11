337fc35bdfa324ed0599a4201a91f54663c7c3d6
ArturoVelasquez <arturovgr@gmail.com>
2015-06-10 23:55:47 -0500

    Update horton2-4.c

--- HW2/horton2-4.c
+++ HW2/horton2-4.c
@@ -4,26 +4,27 @@ int main(void)
 {
 
 /*Inicializa las variables*/
-	float pay = 0.0, hours = 0.0,  cents = 0.0, average = 0.0;    		int bucks = 0;
+	float pay = 0.0, hours = 0.0,  cents = 0.0, average = 0.0;    		
+	int bucks = 0;
 
-/*se pide el valor para el pago semanal*/
+/*Se pide el valor para el pago semanal*/
 	printf("\nIntrodusca el pago semanal: ");	
 	scanf("%f", &pay);
 
-/*se pide la cantidad de horas trabajadas*/
+/*Se pide la cantidad de horas trabajadas*/
 	printf("\nIntrodusca las horas trabajadas: ");
 	scanf("%f", &hours);
 
-/*se calcula el promedio de pago por hora*/
+/*Se calcula el promedio de pago por hora*/
 	average=pay/hours;
 
 /*Se calcula el valor entero del promedio a pagar en dolares (solo los valores enteros) */
 	bucks=(int)average;
 
-/*Se calcula el valor de lkos centavos restando del promedio el valor entero*/
+/*Se calcula el valor de los centavos restando del promedio el valor entero*/
 	cents=(average-bucks);
 
-/*se da un valor enetero a los centavos*/
+/*Se da un valor enetero a los centavos*/
 	cents=cents*100;
 
 /*Se retorna el promedio de paga por hora*/
